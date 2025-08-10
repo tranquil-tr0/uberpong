59b381d1764fb552fa4e17991b9e5c308269d9e7
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2025-08-10 03:08:02 -0400

    nix for dj_laser bc im silly

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    fenix,
+  } @ inputs: let
+    system = "x86_64-linux";
+    overlays = [(fenix.overlays.default)];
+    pkgs = import nixpkgs {inherit system overlays;};
+    lib = pkgs.lib;
+
+    rustToolchain = pkgs.fenix.stable.toolchain;
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [alejandra rustToolchain nodejs];
+      RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/src";
+    };
+  };
+}
